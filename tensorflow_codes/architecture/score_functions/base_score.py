9318ed4f6dc7955b1355276ff51a23ff453f5261
CaNucNuong <100041034+Binh707@users.noreply.github.com>
2023-10-30 15:50:02 +0700

    update att

--- tensorflow_codes/architecture/score_functions/base_score.py
+++ tensorflow_codes/architecture/score_functions/base_score.py
@@ -1,6 +1,6 @@
 class BaseScorer:
 
-    def __init__(self, head, relation, tail, mode, W, mask):
+    def __init__(self, head, relation, tail, mode, W = None, mask = None):
         self.head = head
         self.relation = relation
         self.tail = tail
