50f2ae4e0a0fa2da6482567332f5847d9cfd7132
CaNucNuong <100041034+Binh707@users.noreply.github.com>
2023-10-30 16:04:44 +0700

    Update TransE.py

--- tensorflow_codes/architecture/score_functions/TransE.py
+++ tensorflow_codes/architecture/score_functions/TransE.py
@@ -4,8 +4,8 @@ from .base_score import BaseScorer
 
 class TransEScorer(BaseScorer):
 
-    def __init__(self, head, relation, tail, mode, gamma):
-        super().__init__(head, relation, tail, mode)
+    def __init__(self, head, relation, tail, mode, W, mask, gamma):
+        super().__init__(head, relation, tail, mode, W, mask)
         self.gamma = gamma
 
     def compute_score(self):
