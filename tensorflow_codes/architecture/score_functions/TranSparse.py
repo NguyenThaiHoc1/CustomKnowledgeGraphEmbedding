a826825e49e245cee7b08fd2634429148be23ec4
CaNucNuong <100041034+Binh707@users.noreply.github.com>
2023-11-01 10:19:28 +0700

    Update TranSparse.py

--- tensorflow_codes/architecture/score_functions/TranSparse.py
+++ tensorflow_codes/architecture/score_functions/TranSparse.py
@@ -10,13 +10,14 @@ class TranSparseScorer(BaseScorer):
 
     def compute_score(self):
         p_head = tf.matmul(self.head, (self.mask * self.W))
-        p_head = tf.linalg.normalize(p_head, ord=2, axis=-1)[0]
+        # p_head = tf.linalg.normalize(p_head, ord=2, axis=-1)[0]
         
         p_tail = tf.matmul(self.tail, (self.mask * self.W))
-        p_tail = tf.linalg.normalize(p_tail, ord=2, axis=-1)[0]
+        # p_tail = tf.linalg.normalize(p_tail, ord=2, axis=-1)[0]
         
-        relation = tf.linalg.normalize(self.relation, ord=2, axis=-1)[0]
+        # relation = tf.linalg.normalize(self.relation, ord=2, axis=-1)[0]
+        relation = self.relation
 
         score = p_head + relation - p_tail
-        score = self.gamma - tf.norm(score, ord=1, axis=2) ** 2
+        score = self.gamma - tf.norm(score, ord=1, axis=2)
         return score
