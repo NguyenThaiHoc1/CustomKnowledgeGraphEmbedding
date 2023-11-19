79a811908306fea36a9445fdbf16ff4aeb1e1b2f
CaNucNuong <100041034+Binh707@users.noreply.github.com>
2023-11-19 21:38:28 +0700

    Update TransR.py

--- tensorflow_codes/architecture/score_functions/TransR.py
+++ tensorflow_codes/architecture/score_functions/TransR.py
@@ -4,17 +4,19 @@ from .base_score import BaseScorer
 
 class TranSparseScorer(BaseScorer):
 
-    def __init__(self, head, relation, tail, mode, W, mask, gamma):
+    def __init__(self, head, relation, tail, mode, W, gamma):
         super().__init__(head, relation, tail, mode)
         self.gamma = gamma
 
     def compute_score(self):
-        p_head = tf.matmul(self.head, (self.mask * self.W))
+        p_head = tf.matmul(self.head, self.W)
         # p_head = tf.linalg.normalize(p_head, ord=2, axis=-1)[0]
-        p_tail = tf.matmul(self.tail, (self.mask * self.W))
+        
+        p_tail = tf.matmul(self.tail, self.W)
         # p_tail = tf.linalg.normalize(p_tail, ord=2, axis=-1)[0]
-        # relation = tf.linalg.normalize(self.relation, ord=2, axis=-1)[0]
+        
         relation = self.relation
+        # relation = tf.linalg.normalize(self.relation, ord=2, axis=-1)[0]
 
         score = p_head + relation - p_tail
         score = self.gamma - tf.norm(score, ord=1, axis=2)
